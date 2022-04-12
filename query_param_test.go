58b938af447f3b03a2d11f0db61068c696781655
Rizky Darmawan <darmawanrizky43@gmail.com>
2022-04-12 11:15:57 +0700

    Added test multiple value parameter

--- query_param_test.go
+++ query_param_test.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -68,6 +69,39 @@ func TestMultipleQueryParameter(t *testing.T) {
 
 	// (3) Call function SayHello with parameter recorder for writer, request for recorder
 	MultipleQueryParameter(recorder, request)
+
+	// (4) Get response from recorder
+	response := recorder.Result()
+
+	// (5) convert response to string
+	body, err := io.ReadAll(response.Body)
+
+	// (6) IF error
+	if err != nil {
+		panic(err)
+	}
+
+	// (7)  IF no error, print response
+	fmt.Println(string(body))
+}
+
+func MultipleParameterValues(writer http.ResponseWriter, request *http.Request) {
+	// (1) Create variable with value URL Query
+	query := request.URL.Query()
+	// (2) Get multiple values parameter
+	names := query["name"]
+	// (3) Print, and join all string from parameter
+	fmt.Fprint(writer, strings.Join(names, " "))
+}
+
+func TestMultipleValues(t *testing.T) {
+	// (1) Create var request with httptest.NewRequest(Method, "Host with multiple values parameter", body)
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/hello?name=Rizky&name=Darmawan", nil)
+	// (2) Create variable recoreder for writer
+	recorder := httptest.NewRecorder()
+
+	// (3) Call function SayHello with parameter recorder for writer, request for recorder
+	MultipleParameterValues(recorder, request)
 
 	// (4) Get response from recorder
 	response := recorder.Result()
