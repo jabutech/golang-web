2f88635665b05a2f840b72b05fb75c592673f284
Rizky Darmawan <darmawanrizky43@gmail.com>
2022-04-12 10:52:22 +0700

    Added function test for get multiple parameters

--- query_param_test.go
+++ query_param_test.go
@@ -47,3 +47,39 @@ func TestQueryParameter(t *testing.T) {
 	// (7)  IF no error, print response
 	fmt.Println(string(body))
 }
+
+// Function for handle multiple query parameter
+func MultipleQueryParameter(writer http.ResponseWriter, request *http.Request) {
+	// (1) Get query parameter "first_name"
+	firstName := request.URL.Query().Get("first_name")
+	// (2) Get query parameter "last_name"
+	lastName := request.URL.Query().Get("last_name")
+
+	// (3) Print
+	fmt.Fprintf(writer, "Hello %s %s", firstName, lastName)
+}
+
+// Function test multiple parameter
+func TestMultipleQueryParameter(t *testing.T) {
+	// (1) Create var request with httptest.NewRequest(Method, "Host with multiplequery parameter", body)
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/hello?first_name=Rizky&last_name=Darmawan", nil)
+	// (2) Create variable recoreder for writer
+	recorder := httptest.NewRecorder()
+
+	// (3) Call function SayHello with parameter recorder for writer, request for recorder
+	MultipleQueryParameter(recorder, request)
+
+	// (4) Get response from recorder
+	response := recorder.Result()
+
+	// (5) convert response to string
+	body, err := io.ReadAll(response.Body)
+
+	// (6) IF error
+	if err != nil {
+		panic(err)
+	}
+
+	// (7)  IF no error, print response
+	fmt.Println(string(body))
+}
